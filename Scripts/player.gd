d79b12ebb4817c09849b8839057f15e1eac02273
AlvaroMendesSpeedy <75328262+AlvaroMendesSpeedy@users.noreply.github.com>
2021-01-22 11:31:25 -0300

    finalização do projeto

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,11 @@
 extends KinematicBody2D
 
 export var vel = 200 #velocidade
+var key = false
+var perto_chave = false
+var porta = false
+onready var chave = get_parent().get_node("chave")
+onready var node_porta = $"../porta"
 
 func _ready():
 	pass
@@ -17,3 +22,28 @@ func _process(delta):
 		dir.x = 1
 	
 	move_and_slide(dir * vel)
+
+func _input(event):
+	if key and perto_chave and Input.is_key_pressed(KEY_X):
+		chave.queue_free()
+		perto_chave = false
+		
+	if porta and key and Input.is_key_pressed(KEY_X):
+		node_porta.queue_free()
+
+func _on_area_body_entered(body):
+	if body.name == "chave":
+		perto_chave = true
+		key = true
+		
+	if body.name == "porta":
+		porta = true
+	
+
+
+func _on_area_body_exited(body):
+	if body.name == "porta":
+		porta = false
+	
+	if body.name == "key":
+		key = false
